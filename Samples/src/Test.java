7579eb128e39f3300e5de91342475ef77bdb7b27
Kavitha <kavitha_kappera@infosys.com>
2018-08-07 18:48:17 +0530

    for jenkins

--- Samples/src/Test.java
+++ Samples/src/Test.java
@@ -6,6 +6,6 @@ public static void main(String args[])
 Base obj= new Derived();
 obj.display();
 obj.print();
-System.out.println("HI from EGIT 1");
+System.out.println("HI from EGIT 1 for JEnkins");
 }
 }
